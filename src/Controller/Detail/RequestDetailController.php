480c61517a992ebaffa033a621482f7281b384f7
No1s-yTaniguchi <39228633+No1s-Taniguchi@users.noreply.github.com>
2018-11-30 17:11:05 +0900

    teamproject 詳細、依頼詳細画面

---
+++ src/Controller/Detail/RequestDetailController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use App\Model\DetailModel;
+
+class RequestDetailController extends AppController {
+    public function index() {
+        $model = $this->loadModel('Detail');
+    }
+    public function view() {
+        $client = 'データベース：依頼主';
+        $content = 'データベース：内容';
+        $time = 'データベース：時間';
+        $compensation = 'データベース：報酬';
+        $remarks = 'データベース：備考';
+        $this->set('client', $client);
+        $this->set('content', $content);
+        $this->set('time', $time);
+        $this->set('compensation', $compensation);
+        $this->set('remarks', $remarks);
+
+        $this->render('requestDetail');
+    }
+}
