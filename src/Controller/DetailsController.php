eafd81eb421c149cbe40e59aed1b602928e6f1d9
No1s-yTaniguchi <39228633+No1s-Taniguchi@users.noreply.github.com>
2019-03-07 01:28:40 +0900

    teamProject details

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -1,58 +1,31 @@
 <?php
-namespace App\Controller;
 
-use App\Controller\AppController;
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\AppController;
 use Cake\ORM\TableRegistry;
 
 class DetailsController extends AppController {
     public function index() {
-        $id = $this->request->query('id');
-        $id = 2;
-        $postId = 2;
-//        $this->loadModel('Favorites');
-
-        /*$favoritesTable = TableRegistry::get('favorites');
-        $newfav = $favoritesTable->get(1);
-        $detailsTable = TableRegistry::get('details');
-        $newfav = $detailsTable->get(1);
-
-        $newfavorites = $favoritesTable->find()->where(["user_id = " => $id]);
-
-        $newfavorites->active_flag = 0;
-        $favoritesTable->save($newfavorites);
-        $this->set('fav',$newfavorites);
-
-
-        $favorites = $this->Favorites->find('all',array(
-            'conditions' => array('user_id' => $id)
-        ));
-        foreach($favorites as $fav) {
-            $fav = $fav->active_flag;
-        }
-        $this->set('fav',$fav);
-        $this->set('favorites',$favorites);
-        $this->render('details');
-*/
+        $id = $this->request->params['pass'][0];
+        $fav = '';
         if (!empty($_POST['fav_off'])) {
             $fav = 0;
             $this->set('fav',$fav);
-            $this->render('details');
         }
         if (!empty($_POST['fav_on'])) {
             $fav = 1;
             $this->set('fav',$fav);
-            $this->render('details');
         }
         if (!empty($_POST['ask'])) {
             $fav = 1;
             $this->set('fav',$fav);
-            $this->render('details');
         }
 
+        $this->set('fav',$fav);
         $results = $this->Details->find('all',array(
             'conditions' => array('id' => $id)
         ));
-
         $this->set('results',$results);
         $this->render('details');
 
